cb74ca7c8ad9ca5383003ae6020bc22fac99e505
jgudelj23 <jgudelj23@student.foi.hr>
2026-01-22 10:27:59 +0100

    Dodali smo build_walkable u razine.py koji definira točan raspored prohodnih polja (11x17).

--- razine.py
+++ razine.py
@@ -48,3 +48,30 @@ class GameMap:
 
     def in_bounds(self, p: Pos) -> bool:
         return 0 <= p.x < self.w and 0 <= p.y < self.h
+def build_walkable() -> set[Pos]:
+    W, H = 11, 17
+    b = MapBuilder(W, H)
+
+    b.add_vertical(5, 0, 2)
+    b.add_horizontal(3, 2, 7)
+    b.add_vertical(2, 3, 7)
+    b.add_vertical(7, 3, 5)
+    b.add_vertical(6, 5, 7)
+    b.add_horizontal(5, 6, 9)
+    b.add_vertical(9, 5, 12)
+
+    b.add_horizontal(7, 0, 4)
+    b.add_vertical(0, 7, 10)
+    b.add_vertical(4, 7, 10)
+    b.add_cells(Pos(6, 7), Pos(9, 7))
+
+    b.add_vertical(3, 10, 13)
+    b.add_horizontal(12, 3, 5)
+    b.add_vertical(5, 12, 13)
+
+    b.add_horizontal(12, 7, 10)
+    b.add_vertical(10, 12, 15)
+    b.add_vertical(7, 12, 16)
+    b.add_horizontal(15, 7, 10)
+
+    return b.walkable
